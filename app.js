d29c85a7f07d2bb8eb88cb3fc9317af04728ab74
roberto <rueda.roberto89@gmail.com>
2017-08-10 16:07:42 -0500

    function for random number

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 $("document").ready(function(){
+
+//genera el número aleatorio
+function aleatorio(){
+  var cifra = []
+  for (i=0; i<4; i++){
+    	cifra[i] = Math.floor(Math.random()*(9+1))
+    //evita que se repita un número
+    for(ii=0;ii<i;ii++){
+		  if(cifra[i]==cifra[ii]){i-=1;break}
+  	}
+  }
+  return cifra.join("")
+  }
+//genera el submit cuando se oprime enter
   $("input").keypress(function(event) {
       if (event.which == 13) {
           event.preventDefault();
@@ -6,3 +20,8 @@ $("document").ready(function(){
     }
   });
 })
+//cierra el document
+};
+
+
+
